8fae109877106cdc47b93baec5533c98c00dea04
Mohcen <hugoboy2311@gmail.com>
2023-01-04 19:05:51 +0100

    /package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './.env'})
 
 const PORT = process.env.PORT || 5000
 
