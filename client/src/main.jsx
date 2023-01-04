dc107785476d5b3f8cceb03aa4501d385911bd10
Mohcen23 <hugoboy2311@gmail.com>
2023-01-04 19:06:44 +0100

    Delete client directory

--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Index from './Index'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-)
